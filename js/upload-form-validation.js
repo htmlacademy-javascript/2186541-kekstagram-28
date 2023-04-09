1605478f960dc35b703511372387f04dc1f90802
ancher-muancher <annachernavina29@gmail.com>
2023-04-09 16:51:33 +0300

    task11and12

--- js/upload-form-validation.js
+++ js/upload-form-validation.js
@@ -6,6 +6,7 @@ const TEXT_ERROR = 'Неверные хэштэги';
 
 const uploadForm = document.querySelector('.img-upload__form');
 const textHashtags = document.querySelector('.text__hashtags');
+const submitButton = document.querySelector('#upload-submit');
 
 const pristine = new Pristine(uploadForm, {
   classTo: 'img-upload__field-wrapper',
@@ -27,14 +28,26 @@ const validateHashtags = (value) => {
 
 pristine.addValidator(textHashtags, validateHashtags, TEXT_ERROR);
 
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Идет отправка....';
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
 const setUserFormSubmit = (onSuccess, onError) => {
   uploadForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
     const isValid = pristine.validate();
     if (isValid) {
+      blockSubmitButton();
       sendData(new FormData(evt.target))
         .then(onSuccess)
-        .catch(onError);
+        .catch(onError)
+        .finally(unblockSubmitButton);
     }
   });
 };
